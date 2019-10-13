7ef87b53e3b5a4442a7975352f71e3376dd3b25d
KhesimReid <khesim.reid@gmail.com>
2019-10-13 12:16:54 +0100

    Updated annotations on variables in User entity.

--- travelly/src/main/java/com/example/travelly/model/User.java
+++ travelly/src/main/java/com/example/travelly/model/User.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -13,7 +14,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull
     private String username;
+
+    @NotNull
     private String password;
+
+    @NotNull
     private String email;
 }
