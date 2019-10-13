d22bd24bcf39b8594c8253763d44c6f3e1cbbeba
MilenOrfeev <milen.orfeev@abv.bg>
2019-10-13 10:15:36 +0100

    initial commit 2

---
+++ travelly/src/test/java/com/example/travelly/TravellyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.travelly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravellyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
