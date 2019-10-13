0a67b8765a0bd8e32a20f74eedeb4e44753e4a51
KhesimReid <khesim.reid@gmail.com>
2019-10-13 14:43:07 +0100

    Added InitialDataLoader to populate database on application start.

---
+++ travelly/src/main/java/com/example/travelly/config/data/InitialDataLoader.java
@@ -0,0 +1,173 @@
+package com.example.travelly.config.data;
+
+import com.example.travelly.model.Listing;
+import com.example.travelly.repository.ListingRepository;
+import com.example.travelly.service.ListingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Profile({ "default", "test" })
+public class InitialDataLoader implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final static Logger log = LoggerFactory.getLogger(InitialDataLoader.class);
+
+    @Autowired
+    private ListingService listingService;
+
+    @Autowired
+    private ListingRepository listingRepository;
+
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+
+//        if (listingService.count() > 0) {
+//            log.info("Database already populated. Skipping data initialization.");
+//            return;
+//        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm");
+
+        Date alphaDate1 = null;
+        Date alphaDate2 = null;
+
+
+        Listing listingA = new Listing();
+        listingA.setDestination("New York, New York");
+        try {
+            alphaDate1 = dateFormat.parse("2020-06-11");
+            alphaDate2= timeFormat.parse("2020-06-18");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        listingA.setStartDate(alphaDate1);
+        listingA.setEndDate(alphaDate2);
+        listingA.setCapacity(100);
+        listingRepository.save(listingA);
+        log.info("Added listing (" + listingA.getId() + ") ");
+
+        Listing listingB = new Listing();
+        listingB.setDestination("Paris, France");
+        try {
+            alphaDate1 = dateFormat.parse("2020-05-01");
+            alphaDate2= timeFormat.parse("2020-05-31");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        listingB.setStartDate(alphaDate1);
+        listingB.setEndDate(alphaDate2);
+        listingB.setCapacity(100);
+        listingRepository.save(listingB);
+        log.info("Added listing (" + listingB.getId() + ") ");
+
+
+        Listing listingC = new Listing();
+        listingC.setDestination("London, England");
+        try {
+            alphaDate1 = dateFormat.parse("2020-01-01");
+            alphaDate2= timeFormat.parse("2020-01-07");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        listingC.setStartDate(alphaDate1);
+        listingC.setEndDate(alphaDate2);
+        listingC.setCapacity(100);
+        listingRepository.save(listingC);
+        log.info("Added listing (" + listingC.getId() + ") ");
+
+        Listing listingD = new Listing();
+        listingD.setDestination("Cozumel, Mexico");
+        try {
+            alphaDate1 = dateFormat.parse("2020-09-18");
+            alphaDate2= timeFormat.parse("2020-09-25");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        listingD.setStartDate(alphaDate1);
+        listingD.setEndDate(alphaDate2);
+        listingD.setCapacity(100);
+        listingRepository.save(listingD);
+        log.info("Added listing (" + listingD.getId() + ") ");
+
+        Listing listingE = new Listing();
+        listingE.setDestination("Stockholm, Sweden");
+        try {
+            alphaDate1 = dateFormat.parse("2020-12-20");
+            alphaDate2= timeFormat.parse("2021-01-07");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        listingE.setStartDate(alphaDate1);
+        listingE.setEndDate(alphaDate2);
+        listingE.setCapacity(100);
+        listingRepository.save(listingE);
+        log.info("Added listing (" + listingE.getId() + ") ");
+
+        Listing listingF = new Listing();
+        listingF.setDestination("Las Vegas, Nevada");
+        try {
+            alphaDate1 = dateFormat.parse("2020-12-20");
+            alphaDate2= timeFormat.parse("2021-01-07");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        listingF.setStartDate(alphaDate1);
+        listingF.setEndDate(alphaDate2);
+        listingF.setCapacity(100);
+        listingRepository.save(listingF);
+        log.info("Added listing (" + listingF.getId() + ") ");
+
+        Listing listingG = new Listing();
+        listingG.setDestination("Barbados");
+        try {
+            alphaDate1 = dateFormat.parse("2020-12-20");
+            alphaDate2= timeFormat.parse("2021-01-07");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        listingG.setStartDate(alphaDate1);
+        listingG.setEndDate(alphaDate2);
+        listingG.setCapacity(100);
+        listingRepository.save(listingG);
+        log.info("Added listing (" + listingG.getId() + ") ");
+
+        Listing listingH = new Listing();
+        listingH.setDestination("Trinidad");
+        try {
+            alphaDate1 = dateFormat.parse("2020-12-20");
+            alphaDate2= timeFormat.parse("2021-01-07");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        listingH.setStartDate(alphaDate1);
+        listingH.setEndDate(alphaDate2);
+        listingH.setCapacity(100);
+        listingRepository.save(listingH);
+        log.info("Added listing (" + listingH.getId() + ") ");
+
+        Listing listingI = new Listing();
+        listingI.setDestination("Barcelona, Spain");
+        try {
+            alphaDate1 = dateFormat.parse("2020-12-20");
+            alphaDate2= timeFormat.parse("2021-01-07");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        listingI.setStartDate(alphaDate1);
+        listingI.setEndDate(alphaDate2);
+        listingI.setCapacity(100);
+        listingRepository.save(listingI);
+        log.info("Added listing (" + listingI.getId() + ") ");
+    }
+}
